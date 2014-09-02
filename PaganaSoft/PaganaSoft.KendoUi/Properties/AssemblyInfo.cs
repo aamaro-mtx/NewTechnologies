0750053d694ea54462a4b119d438ecc29d0ec445
Abel Amaro <amarotux@gmail.com>
2014-09-02 17:51:51 -0400

    Kendo UI Core added

---
+++ PaganaSoft/PaganaSoft.KendoUi/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PaganaSoft.KendoUi")]
+[assembly: AssemblyDescription("PaganaSoft Web")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("PaganaSoft")]
+[assembly: AssemblyProduct("PaganaSoft.KendoUi")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("PagamaSoft")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2186b479-bc3f-4516-b96a-39c8d5d7d66a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PaganaSoft/PaganaSoft.KendoUi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PaganaSoft.KendoUi.Startup))]
+namespace PaganaSoft.KendoUi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
